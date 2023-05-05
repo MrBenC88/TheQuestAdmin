153129852041e1c90e30cbd79ba044b42eb2f920
mrbenc88 <mrbenc88@students.cs.ubc.ca>
2023-05-05 00:25:16 -0700

    automatically populate mongodb

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,11 +2,27 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import connectToDB from "../../../../lib/mongodb";
+import clientPromise from "../../../../lib/mongodb";
+
+const callbacks = {
+  session: async (session, user) => {
+    try {
+      const client = await clientPromise;
+      if (typeof client.db !== "function") {
+        console.error("client.db is not a function");
+        return Promise.resolve(session);
+      }
+      console.log("CHECK!!!!!:" + client);
+
+      if (user) console.log("OUR USER:" + user);
+    } catch (error) {
+      console.error("Error updating user details in MongoDB:", error);
+    }
+    return Promise.resolve(session);
+  },
+};
 
 export default async function auth(req, res) {
-  //   const db = await connectToDB();
-
   return NextAuth(req, res, {
     providers: [
       GoogleProvider({
@@ -16,18 +32,13 @@ export default async function auth(req, res) {
       // Add other providers if needed
     ],
 
-    // adapter: MongoDBAdapter({
-    //   db,
-    // }),
+    adapter: MongoDBAdapter(clientPromise),
 
     pages: {
       // Override default NextAuth pages as needed
     },
 
-    callbacks: {
-      // Customize NextAuth callbacks as needed
-    },
-
+    callbacks,
     // Enable debug messages in development
     debug: process.env.NODE_ENV === "development",
   });
