5c10b30a301ffbe0eaaa54ce1977987c0192980f
mrbenc88 <mrbenc88@students.cs.ubc.ca>
2023-05-07 00:57:34 -0700

    added separate mongoose connection to enforce mongoose schemas

--- src/pages/api/quests.js
+++ src/pages/api/quests.js
@@ -1,50 +1,50 @@
 //   Quest API
 // pages/api/quests.js
-import { connectToDatabase } from "../../../lib/mongodb";
-// import Quest from "./models/Quest";
+import { connect, disconnect } from "../../../lib/mongoose";
+import Quest from "./models/Quest";
 
 export default async function handler(req, res) {
-  const db = await connectToDatabase();
+  await connect();
 
-  switch (req.method) {
-    case "GET":
-      // Fetch global quests
-      try {
-        const quests = await db
-          .collection("quests")
-          .find({ questPermissions: "public" })
-          .toArray();
-        res.status(200).json({ quests });
-      } catch (error) {
-        console.error(error);
-        res
-          .status(500)
-          .json({ error: "An error occurred while fetching the quests" });
-      }
-      break;
-
-    case "POST":
-      // Create a new global quest
-      try {
-        if (!req.body) {
-          throw new Error("Request body is missing");
+  try {
+    switch (req.method) {
+      case "GET":
+        try {
+          const quests = await Quest.find({ questPermissions: "public" });
+          res.status(200).json({ quests });
+        } catch (error) {
+          console.error(error);
+          res
+            .status(500)
+            .json({ error: "An error occurred while fetching the quests" });
         }
+        break;
 
-        const newQuest = req.body;
+      case "POST":
+        try {
+          if (!req.body) {
+            throw new Error("Request body is missing");
+          }
 
-        const result = await db.collection("quests").insertOne(newQuest);
+          const newQuest = req.body;
 
-        res.status(201).json({ quest: result });
-      } catch (error) {
-        console.error(error);
-        res
-          .status(500)
-          .json({ error: "An error occurred while creating the quest" });
-      }
-      break;
+          const quest = new Quest(newQuest);
+          await quest.save();
 
-    default:
-      res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
+          res.status(201).json({ quest });
+        } catch (error) {
+          console.error(error);
+          res
+            .status(500)
+            .json({ error: "An error occurred while creating the quest" });
+        }
+        break;
+
+      default:
+        res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+  } finally {
+    await disconnect();
   }
 }
