7ed6fe0bf0863c1f748266e348883ee1037c1c6a
mrbenc88 <mrbenc88@students.cs.ubc.ca>
2023-05-07 20:09:40 -0700

    create UserQuest mongoose schema

---
+++ src/pages/api/models/UserQuest.js
@@ -0,0 +1,74 @@
+// models/UserQuest.js
+// Model for UserQuest which is the collection that users add quests from global quest collection to their profile
+
+import mongoose from "mongoose";
+
+const proofSchema = new mongoose.Schema(
+  {
+    type: {
+      type: String,
+      enum: ["photo", "video", "text", "auto"],
+      required: true,
+    },
+    value: { type: String, required: true },
+    submittedAt: { type: Date, default: Date.now },
+  },
+  { _id: false }
+);
+
+const taskSubmissionSchema = new mongoose.Schema({
+  taskId: { type: mongoose.Schema.Types.ObjectId, ref: "Task" },
+  completed: { type: Boolean, default: false },
+  proof: proofSchema,
+  createdAt: { type: Date, default: Date.now },
+});
+
+// This schema is for the task submission.
+// It will be when a user submits a task for a quest we update this.
+const submissionBatchSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  questId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Quest",
+  },
+  createdAt: { type: Date, required: true },
+  taskSubmissions: [taskSubmissionSchema],
+});
+
+// Upon clicking into a quest from their userquest list
+// we will need to fetch the quest details from the global quest collection
+// with the quest id from the userquest collection
+// taskSubmissions will be an array of the batchSubmission
+const userQuestSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  questId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Quest",
+  },
+  startDate: { type: Date, default: Date.now },
+  endDate: { type: Date, required: false },
+  userQuestStatus: {
+    type: String,
+    enum: ["complete", "incomplete", "inprogress", "cancelled"],
+    default: "inprogress",
+  },
+  points: { type: Number, default: 0 },
+  streak: { type: Number, default: 0 },
+  lastSubmitted: { type: Date },
+  taskSubmissions: [submissionBatchSchema],
+});
+
+let UserQuest;
+try {
+  UserQuest = mongoose.model("UserQuest");
+} catch (error) {
+  UserQuest = mongoose.model("UserQuest", userQuestSchema);
+}
+
+export default UserQuest;
